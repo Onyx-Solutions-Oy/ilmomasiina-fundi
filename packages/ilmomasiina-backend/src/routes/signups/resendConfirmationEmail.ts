c989e1410f3be479f486d9f34e6b88bcd010b73e
enoquemaculuba <96523554+enoquemaculuba@users.noreply.github.com>
2024-02-09 19:11:34 +0200

    Add resendConfirmationEmail endpoint

---
+++ packages/ilmomasiina-backend/src/routes/signups/resendConfirmationEmail.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-await-in-loop */
+import { FastifyReply, FastifyRequest } from 'fastify';
+import sendSignupConfirmationMail from 'src/mail/signupConfirmation';
+import { Signup } from 'src/models/signup';
+
+import { NoSuchSignup } from './errors';
+
+/** Requires admin authentication */
+export default async function resendConfirmationEmails(
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<void> {
+  const { ids } = request.body as { ids: string[] };
+  console.log('Resending confirmation emails for signups', request.body);
+  for (const id of ids) {
+    const signup = await Signup.scope('active').findByPk(id, {
+      attributes: ['id', 'quotaId', 'confirmedAt', 'firstName', 'lastName', 'email', 'language'],
+    });
+    if (signup === null) {
+      throw new NoSuchSignup('Signup expired or already deleted');
+    }
+    await sendSignupConfirmationMail(signup);
+  }
+  reply.status(204);
+}
